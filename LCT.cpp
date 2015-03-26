cfab32e6fdb3bbfb7667f4a6c7838da0e6d8a76c
lowsfish <low.s.fish@gmail.com>
2015-03-26 22:55:31 +0800

    LCT.cpp

--- LCT.cpp
+++ LCT.cpp
@@ -1,130 +1,79 @@
-//bzoj 2631
+//bzoj 3282
 #include<cstdio>
 #include<algorithm>
-#include<vector>
-const int MOD=51061;
-const int MAXN=100000+10;
-typedef long long int64;
-struct Mark{
-	int64 mul,add;
-	Mark(int64 mu,int64 ad):mul(mu),add(ad) {}
-	Mark() {mul=1;add=0;}
-	inline bool needPush() {return mul!=1 || add!=0;}
-};
-Mark operator*(Mark a,Mark b)
-{
-	return Mark(a.mul*b.mul%MOD,(a.add*b.mul+b.add)%MOD);
-}
+#define rep(i,n) for(int i=0;i<n;++i)
+const int MAXN=300000+10;
+const int INF=~0U>>1;
 struct Node{
-	int s;
-	bool rev,isRoot;
-	Mark m;
-	int64 sum,v;
-	Node* p,*fa,*ch[2];
-	Node() {s=isRoot=sum=0;}
-	inline bool d()const {return p->ch[1]==this;}
+	Node* p,*ch[2];
+	int v,xsum;
+	bool rev;
+	Node()
+	{
+		v=xsum=rev=0;
+	}
+	inline void revIt()
+	{
+		std::swap(ch[0],ch[1]);
+		rev^=1;
+	}
+	inline void maintain()
+	{
+		xsum=ch[0]->xsum^ch[1]->xsum^v;
+	}
 	inline void setc(Node* t,bool d)
 	{
 		ch[d]=t;
 		t->p=this;
 	}
-	inline void maintain()
+	inline bool d() const
 	{
-		s=ch[0]->s+ch[1]->s+1;
-		sum=(ch[0]->sum+ch[1]->sum+v)%MOD;
+		return p->ch[1]==this;
 	}
-	inline void revIt()
-	{
-		rev^=1;
-		std::swap(ch[0],ch[1]);
-	}
-	inline void apply(Mark a)
-	{
-		m=m*a;
-		sum=(sum*a.mul+a.add*s)%MOD;
-		v=(v*a.mul+a.add)%MOD;
-	}
+	inline bool isRoot() const;
 	inline void pushdown();
-	inline void setRoot(Node* f);
 }Tnull,*null=&Tnull;
-Node mem[MAXN],*C=mem;
 inline void Node::pushdown()
 {
 	if(rev)
 	{
-		if(ch[0]!=null) ch[0]->revIt();
-		if(ch[1]!=null) ch[1]->revIt();
 		rev=0;
-	}
-	if(m.needPush())
-	{
-		if(ch[0]!=null) ch[0]->apply(m);
-		if(ch[1]!=null) ch[1]->apply(m);
-		m=Mark();
+		rep(i,2) if(ch[i]!=null)
+			ch[i]->revIt();
 	}
 }
-inline void Node::setRoot(Node* f)
+inline bool Node::isRoot() const
 {
-	isRoot=1;
-	fa=f;
-	p=null;
+	return p==null || (p->ch[0]!=this && p->ch[1]!=this);
 }
-inline Node* newNode(int v)
-{
-	C->p=C->fa=C->ch[0]=C->ch[1]=null;
-	C->s=1;
-	C->m=Mark();
-	C->v=C->sum=v;
-	C->isRoot=1;
-	C->rev=0;
-	return C++;
-}
-inline void rot(Node* t)
+inline void rot(Node *t)
 {
 	Node* p=t->p;
 	p->pushdown();t->pushdown();
 	bool d=t->d();
-	p->p->setc(t,p->d());
+	if(!p->isRoot())p->p->setc(t,p->d());
+	else t->p=p->p;
 	p->setc(t->ch[d^1],d);
 	t->setc(p,d^1);
 	p->maintain();
-	if(p->isRoot)
-	{
-		p->isRoot=0;
-		t->isRoot=1;
-		t->fa=p->fa;
-	}
 }
-void pushTo(Node* t)
+inline void splay(Node* t)
 {
-	static Node* stk[MAXN];
-	int top=0;
-	while(t!=null)
-	{
-		stk[top++]=t;
-		t=t->p;
-	}
-	while(top) stk[--top]->pushdown();
-}
-void splay(Node* t,Node* f=null)
-{
-//	pushTo(t);
 	t->pushdown();
-	while(t->p!=f)
+	while(!t->isRoot())
 	{
-		if(t->p->p==f) rot(t);
+		if(t->p->isRoot()) rot(t);
 		else t->d()==t->p->d()?(rot(t->p),rot(t)):(rot(t),rot(t));
 	}
 	t->maintain();
 }
-Node* access(Node* u)
+inline void access(Node* u)
 {
-	for(Node* v=null;u!=null;v=u,u=u->fa)
+	for(Node* v=null;u!=null;v=u,u=u->p)
 	{
 		splay(u);
-		u->ch[1]->setRoot(u);
 		u->setc(v,1);
-		v->fa=u;
+		u->maintain();
 	}
 }
 inline void makeRoot(Node* u)
@@ -133,85 +82,71 @@ inline void makeRoot(Node* u)
 	splay(u);
 	u->revIt();
 }
-inline void delEdge(Node* u,Node* v)
+inline Node* getRoot(Node* u)
 {
+	for(;u->p!=null;u=u->p);
+	return u;
+}
+inline void link(Node* u,Node* v)
+{
+	if(getRoot(u)==getRoot(v)) return;
+	makeRoot(u);
+	u->p=v;
+}
+inline void cut(Node* u,Node* v)
+{
+	if(u==v || getRoot(u)!=getRoot(v)) return;
 	makeRoot(u);
 	access(v);
-	splay(u);
-	u->setc(null,1);
-	u->maintain();
-	v->fa=v->p=null;
-	v->isRoot=1;
+	splay(v);
+	if(v->ch[0]==u)
+	{
+		u->p=null;
+		v->setc(null,0);
+		v->maintain();
+	}
 }
-inline void addEdge(Node* u,Node* v)
+inline void change(Node* u,int v)
 {
-	makeRoot(v);
-	v->fa=u;
+	access(u);
+	splay(u);
+	u->v=v;
+	u->maintain();
 }
-inline void getPath(Node* u,Node* v)
+inline int query(Node* u,Node* v)
 {
 	makeRoot(u);
 	access(v);
 	splay(v);
+	return v->xsum;
 }
-inline void modify(Node* u,Node* v,Mark a)
+Node mem[MAXN],*C=mem;
+inline Node* newNode(int x)
 {
-	getPath(u,v);
-	v->apply(a);
+	C->p=C->ch[0]=C->ch[1]=null;
+	C->v=C->xsum=x;
+	C->rev=0;
+	return C++;
 }
-inline int queryPath(Node* u,Node* v)
-{
-	getPath(u,v);
-	return v->sum;
-}
-int n,q;
+int n,m;
 Node* pt[MAXN];
-int fa[MAXN],que[MAXN];;
-std::vector<int> G[MAXN];
-typedef std::vector<int>::iterator it;
-void bfs()
-{
-	int head=0,tail=0;
-	que[tail++]=0;fa[0]=-1;
-	while(head<tail)
-	{
-		int u=que[head++];
-		for(it e=G[u].begin();e!=G[u].end();++e)
-			if(*e!=fa[u]) fa[*e]=u,pt[*e]->fa=pt[u],que[tail++]=*e;
-	}
-}
 int main()
 {
-	int u,v,t1,t2;
-	scanf("%d%d",&n,&q);
-	for(int i=0;i<n;++i) pt[i]=newNode(1);
-	for(int i=0;i<n-1;++i)
+//	freopen("1.in","r",stdin);
+//	freopen("1.out","w",stdout);
+	scanf("%d%d",&n,&m);
+	int x;
+	rep(i,n) scanf("%d",&x),pt[i+1]=newNode(x);
+	while(m--)
 	{
-		scanf("%d%d",&u,&v);
-		--u;--v;
-		G[u].push_back(v);
-		G[v].push_back(u);
-//		addEdge(pt[u],pt[v]);
-	}
-	bfs();
-	char s[5];
-	while(q--)
-	{
-		scanf("%s%d%d",s,&u,&v);
-		--u;--v;
-		switch(s[0])
+		int q,u,v;
+		scanf("%d%d%d",&q,&u,&v);
+		switch(q)
 		{
-			case '+':scanf("%d",&t1);
-					 modify(pt[u],pt[v],Mark(1,t1));
-					 break;
-			case '-':scanf("%d%d",&t1,&t2);
-					 delEdge(pt[u],pt[v]);
-					 addEdge(pt[--t1],pt[--t2]);
-					 break;
-			case '*':scanf("%d",&t1);
-					 modify(pt[u],pt[v],Mark(t1,0));
-					 break;
-			default: printf("%d\n",queryPath(pt[u],pt[v]));
+			case 0:printf("%d\n",query(pt[u],pt[v]));break;
+			case 1:link(pt[u],pt[v]);break;
+			case 2:cut(pt[u],pt[v]);break;
+			case 3:change(pt[u],v);break;
 		}
 	}
 	return 0;
