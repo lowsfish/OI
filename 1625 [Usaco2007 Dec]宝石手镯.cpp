718d278477ffae5e8161bd23acfcbeda1fc434d3
lowsfish <low.s.fish@gmail.com>
2015-04-18 22:06:05 +0800

    01背包

---
+++ 1625 [Usaco2007 Dec]宝石手镯.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<algorithm>
+const int MAXM=12880+5;
+const int MAXN=3402+5;
+int n,m;
+int w[MAXN],v[MAXN];
+int dp[MAXM];
+int main()
+{
+	scanf("%d%d",&n,&m);
+	for(int i=0;i<n;++i) scanf("%d%d",w+i,v+i);
+	for(int i=0;i<n;++i)
+		for(int j=m;j>=w[i];--j)
+			dp[j]=std::max(dp[j],dp[j-w[i]]+v[i]);
+	printf("%d\n",dp[m]);
+	return 0;
+}
